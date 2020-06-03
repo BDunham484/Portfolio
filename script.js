acc51c66fb006229abdb9081ba0d367677bf07a2
Brad Dunham <bdunham484@gmail.com>
2020-06-03 02:26:16 -0500

    updated parallax effect

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ const navSlide = () => {
 navSlide();
 
 
+function parallax(element, distance, speed) {
+    const item = document.querySelector(element);
+    item.style.transform = `translateY(${distance * speed}px)`;
+
+}
+
+window.addEventListener("scroll", function(){
+    parallax("header", window.scrollY, 1);
+    parallax('#triangle2', window.scrollY, 0.2);
+    parallax('#triangle3', window.scrollY, 0.05);
+});
+
+
