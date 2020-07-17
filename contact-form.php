2f5805a4d891f09edf0f2c0f1de8e93774eb0874
Brad Dunham <bdunham484@gmail.com>
2020-07-17 16:30:03 -0500

    contact form

---
+++ contact-form.php
@@ -0,0 +1,27 @@
+<?php
+    $name = $_Post['name'];
+    $visitor_email = $_Post['email'];
+    $message = $_Post['message'];
+
+    $email_from = "bdunham484@gmail.com";
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+    
+
+     $to = "bdunham484@gmail.com";
+     
+     $headers = "From: $email_from \r\n";
+
+     $headers .= "Reply-To: $visitor_email \r\n";
+
+     mail($to,$email_subject,$email_body,$headers);
+
+     header("Location: index.html");
+
+
+
+?>
