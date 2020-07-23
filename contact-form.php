f7cb706845d292b0312019dbe5428f862bfc7bf9
Brad Dunham <bdunham484@gmail.com>
2020-07-23 17:48:15 -0500

    updates

--- contact-form.php
+++ contact-form.php
@@ -1,27 +1,30 @@
 <?php
-    $name = $_POST["name"];
-    $visitor_email = $_POST["email"];
-    $message = $_POST["message"];
 
-    $email_from = "bdunham484@gmail.com";
+    // $result="";
+    if(isset($_POST["submit"])){
+        require "phpmailer/PHPMailerAutoload.php";
+        $mail = new PHPMailer;
 
-    $email_subject = "Web Dev Inquiry";
+        $mail->Host="a2plcpnl0717.prod.iad2.secureserver.net";
+        $mail->Port=465;
+        $mail->SMTPAuth=true;
+        $mail->SMTPSecure="ssl";
+        $mail->Username="_mainaccount@brad-dunham.com";
+        $mail->Password="iambraddunham666";
 
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Message: $message.\n";
-    
+        $mail->setFrom($_POST["email"],$_POST["name"]);
+        $mail->addAddress("bdunham484@gmail.com");
+        $mail->addReplyTo($_POST["email"],$_POST["name"]);
 
-     $to = "bdunham484@gmail.com";
-     
-     $headers = "From: $email_from \r\n";
+        $mail->isHTML(true);
+        $mail->Subject="Form Submission: ".$_POST["subject"];
+        $mail->Body="<h1 align=center>Name :".$_POST["name"]."<br>Email: ".$_POST["email"]."<br>Message: ".$_POST["message"]."</h1>";
 
-     $headers .= "Reply-To: $visitor_email \r\n";
-
-     mail($to,$email_subject,$email_body,$headers);
-
-     header("Location: home.html");
-
-
-
+        // if(!$mail->send()){
+        //     $result="Something went wrong. Please try again.";
+        // }
+        // else{
+        //     $result="Thanks ".$-POST["name"]." for contacting us. We'll get back to you soon!";
+        // }
+    }
 ?>
