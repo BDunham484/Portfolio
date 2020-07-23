a6aa8c29fa08006a49ca7944891bca8724be3301
Brad Dunham <bdunham484@gmail.com>
2020-07-23 17:57:29 -0500

    tweeks

--- contact-form.php
+++ contact-form.php
@@ -1,6 +1,6 @@
 <?php
 
-    // $result="";
+    $result="";
     if(isset($_POST["submit"])){
         require "phpmailer/PHPMailerAutoload.php";
         $mail = new PHPMailer;
@@ -8,7 +8,7 @@
         $mail->Host="a2plcpnl0717.prod.iad2.secureserver.net";
         $mail->Port=465;
         $mail->SMTPAuth=true;
-        $mail->SMTPSecure="ssl";
+        $mail->SMTPSecure="tls";
         $mail->Username="_mainaccount@brad-dunham.com";
         $mail->Password="iambraddunham666";
 
@@ -20,11 +20,11 @@
         $mail->Subject="Form Submission: ".$_POST["subject"];
         $mail->Body="<h1 align=center>Name :".$_POST["name"]."<br>Email: ".$_POST["email"]."<br>Message: ".$_POST["message"]."</h1>";
 
-        // if(!$mail->send()){
-        //     $result="Something went wrong. Please try again.";
-        // }
-        // else{
-        //     $result="Thanks ".$-POST["name"]." for contacting us. We'll get back to you soon!";
-        // }
+        if(!$mail->send()){
+            $result="Something went wrong. Please try again.";
+        }
+        else{
+            $result="Thanks ".$_POST["name"]." for contacting us. We'll get back to you soon!";
+        }
     }
 ?>
