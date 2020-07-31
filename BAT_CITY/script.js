59186f79fc2e49eea1fcb154dfc70a33f72f5951
Brad Dunham <bdunham484@gmail.com>
2020-07-31 15:17:24 -0500

    quantity button

--- BAT_CITY/script.js
+++ BAT_CITY/script.js
@@ -32,5 +32,23 @@ $("body").on("click", "label", function(e) {
     console.log(getInputRadio.attr("id"));  
   });
 
+
+ 
+
+
+  function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+  }
   
   
