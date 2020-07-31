b42de41ca0f3011b4ec8762adf5ac72513e601af
Brad Dunham <bdunham484@gmail.com>
2020-07-31 14:03:52 -0500

    size checkbox tweak

--- BAT_CITY/script.js
+++ BAT_CITY/script.js
@@ -27,7 +27,7 @@ navSlide();
 
 $("body").on("click", "label", function(e) {
     var getValue = $(this).attr("for");
-    var goToParent = $(this).parents(".sizes");
+    var goToParent = $(this).parents(".select-size");
     var getInputRadio = goToParent.find("input[id = " + getValue + "]");
     console.log(getInputRadio.attr("id"));  
   });
